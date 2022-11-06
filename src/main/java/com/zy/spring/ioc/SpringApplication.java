892e6e5eda29f1ef53a409d3c81ba641ea3a60dc
zy <1661996964@qq.com>
2022-11-06 15:05:40 +0800

    初始化不会创建对象，调用时会创建

--- src/main/java/com/zy/spring/ioc/SpringApplication.java
+++ src/main/java/com/zy/spring/ioc/SpringApplication.java
@@ -1,16 +1,21 @@
 package com.zy.spring.ioc;
 
 import com.zy.spring.ioc.dao.UserDao;
+import com.zy.spring.ioc.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringApplication {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-   //     UserDao userDao = context.getBean("userDao", UserDao.class);
+        System.out.println("IoC容器已初始化===========");
+//        UserDao userDao = context.getBean("userDao", UserDao.class);
 //        UserDao userDao1 = context.getBean("userDao", UserDao.class);
 //        UserDao userDao2 = context.getBean("userDao", UserDao.class);
 //        UserDao userDao3 = context.getBean("userDao", UserDao.class);
+        UserService userService = context.getBean("userService", UserService.class);
+        UserService userService1 = context.getBean("userService", UserService.class);
+        UserService userService2 = context.getBean("userService", UserService.class);
 
     }
 }
