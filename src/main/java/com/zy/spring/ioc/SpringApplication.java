3d3f711129fea9844ea4f72f625551ca9dbcda03
zy <1661996964@qq.com>
2022-11-06 13:07:59 +0800

    配置bean，在设置scope的情况下，默认为singleton,会在IoC容器启动时创建

---
+++ src/main/java/com/zy/spring/ioc/SpringApplication.java
@@ -0,0 +1,11 @@
+package com.zy.spring.ioc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringApplication {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+
+    }
+}
