b6da2673176e1f1fcb45a57dac1fcc4490af499d
zy <1661996964@qq.com>
2022-11-06 13:10:16 +0800

    配置bean，在设置scope=prototype的情况下，初始化时不创建对象，只有调用时才创建对象

--- src/main/java/com/zy/spring/ioc/SpringApplication.java
+++ src/main/java/com/zy/spring/ioc/SpringApplication.java
@@ -1,11 +1,13 @@
 package com.zy.spring.ioc;
 
+import com.zy.spring.ioc.dao.UserDao;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringApplication {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        UserDao userDao = context.getBean("userDao", UserDao.class);
 
     }
 }
