b0fbb61d6ed2dafb0574f46af39a5017edb0c1e4
zy <1661996964@qq.com>
2022-11-06 13:11:13 +0800

    在设置scope=prototype的情况下，初始化时不创建对象， 只有调用时才创建对象，调用多次则创建多个对象

--- src/main/java/com/zy/spring/ioc/SpringApplication.java
+++ src/main/java/com/zy/spring/ioc/SpringApplication.java
@@ -8,6 +8,9 @@ public class SpringApplication {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
         UserDao userDao = context.getBean("userDao", UserDao.class);
+        UserDao userDao1 = context.getBean("userDao", UserDao.class);
+        UserDao userDao2 = context.getBean("userDao", UserDao.class);
+        UserDao userDao3 = context.getBean("userDao", UserDao.class);
 
     }
 }
